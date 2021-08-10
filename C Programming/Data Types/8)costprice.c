e58d30795e5e4898484520987c3a56f12fd04d90
Burgula Shivaj <shivaji.burgula.sb@gmail.com>
2021-08-10 07:50:36 +0000

    Update 8)costprice.c

--- C Programming/Data Types/8)costprice.c
+++ C Programming/Data Types/8)costprice.c
@@ -2,10 +2,10 @@
 void main()
 
 {
-int P,SP,CP;
-printf("Enter P,SP: ");
-scanf("%d%d",&P,&SP);
-CP=SP-P;
+int P,SP,CP,n;
+printf("Enter P,SP,n: ");
+scanf("%d%d%d",&P,&SP,n);
+CP=(SP-P)/n;
 printf("Cost Price= %d\n",CP);
 
 }
