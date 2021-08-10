471d2998ae4145531f9dcaaa023af8a114b27b69
Burgula Shivaj <shivaji.burgula.sb@gmail.com>
2021-08-10 06:30:35 +0000

    Update 6)rectangle.c

---
+++ C Programming/Data Types/6)rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int length,breadth,A,P;
+printf("Enter Length and Breadth : ");
+scanf("%d%d",&length,&breadth);
+A=length*breadth;
+P=2*(length+breadth);
+printf("Area=%d and perimeter=%d\n",length,breadth);
+}
+
