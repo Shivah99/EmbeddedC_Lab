efdf01213e48196121e7d7038918c383cb28dca7
Burgula Shivaj <shivaji.burgula.sb@gmail.com>
2021-08-10 06:25:37 +0000

    Add new file

---
+++ C Programming/Data Types/3)arithemetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,add,sub,mul,divi;
+    printf("enter m:");
+    scanf("%d%d",&a,&b);
+    add=a+b;
+    sub=a-b;
+    mul=a*b;
+    divi=a/b
+    printf("add=%d,sub=%d,div=%d,mul=%d",add,sub,divi,mul);
+    
+}
