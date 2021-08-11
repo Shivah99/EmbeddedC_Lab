b5b50456ac721bc20df78ac849c1cf4cf0c5fb8c
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-11 12:26:52 +0000

    Update 2) escape_sequences.c

--- C Programming/Variables/2) escape_sequences.c
+++ C Programming/Variables/2) escape_sequences.c
@@ -5,10 +5,9 @@ int main()
 int a;
 scanf("%d",&a);
 printf("%d\n",a);
-printf("%d\b\n",a);
-printf("%d\b\b\n",a);
-printf("%d\b\b\b\n",a);
-
+printf("%d\b \n",a);
+printf("%d\b\b   \n",a);
+printf("%d\b\b\b   \n",a);
 
     return 0;
 }
