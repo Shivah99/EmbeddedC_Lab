8034e6c1567bafce8d89bfab7778628e592f1e12
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-13 05:28:45 +0000

    Update Mobile_service.c

--- C Programming/Conditional Operators/Mobile_service.c
+++ C Programming/Conditional Operators/Mobile_service.c
@@ -4,16 +4,7 @@ int main()
 float n,nbill,bill=250;
     scanf("%f",&n);
     printf("\nEnter the no.of calls made:%f\n",n);
-if(n<=100)
-{
-    printf("The net bill= %f ",bill);
-}
- else 
- {    
- nbill=(bill+(1.25+n-100));
-printf("the net bill for more than 100 calls made:%f",nbill);
- }         
+nbill=(n<=100)?(bill):(bill+(1.25+n-100));
+printf("the net bill for more than 100 calls made:%0.2f",nbill);
 return 0;
 }
-
-
