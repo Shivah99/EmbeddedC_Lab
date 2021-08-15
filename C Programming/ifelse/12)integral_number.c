c25aad856072870c4d52db0e0afb2d1ce8d93180
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-15 19:30:12 +0000

    Add new file

---
+++ C Programming/ifelse/12)integral_number.c
@@ -0,0 +1,17 @@
+
+int main()
+{
+    float n;
+    printf("Enter a number:");
+    scanf("%f",&n);
+    if(n!=0)
+    {
+        printf("\nThe right-most digit of the integral part of the number is:%d",(int)n%10);
+    }
+    else
+    {
+        printf("Enter a valid number");
+    }
+
+    return 0;
+}
