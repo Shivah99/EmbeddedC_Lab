d87ff887afdb7a43d18113b9a1277c7d3cf1e337
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-16 17:58:53 +0000

    Update 3)leap year.c

---
+++ C Programming/LOGICAL OPERATORS/3)leap year.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int year;
+printf("\nEnter the year: ");
+scanf("%d",&year);
+if((year%400==0 && year%4==0) || (year%100!=0 && year%4==0))  
+ printf("%d is a Leap year",year);
+ else
+ printf("not");
+ return 0;
+}
