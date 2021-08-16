3f4cc556d51d762feb4141029525cc3bcf3e7de2
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-16 17:59:55 +0000

    Update 3)leap year.c

--- C Programming/LOGICAL OPERATORS/3)leap year.c
+++ C Programming/LOGICAL OPERATORS/3)leap year.c
@@ -7,6 +7,6 @@ scanf("%d",&year);
 if((year%400==0 && year%4==0) || (year%100!=0 && year%4==0))  
  printf("%d is a Leap year",year);
  else
- printf("not");
+ printf("%d is not a Leap year",year);
  return 0;
 }
