05f0ac9ad6c2c37781c731d32a99c53d44280357
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-17 06:18:01 +0000

    Update 2)positve_neg_zero.c

--- C Programming/ifelse/2)positve_neg_zero.c
+++ C Programming/ifelse/2)positve_neg_zero.c
@@ -2,16 +2,16 @@
 int main()
 {
 int a;
-printf("Ente a "); 
+printf("Enter a "); 
 scanf("%d",&a);
 if(a>0)
 {
     printf("The Entered number is a positive number");
-
-if(a<0)
+}
+else if(a<0)
 {
     printf("The Entered number is a negative number");
-}}
+}
 else
 {
    a=0;
