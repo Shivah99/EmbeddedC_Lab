70c839a4308759b4d0bffd52bc8c87b618a2f412
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-18 16:42:23 +0000

    Add new file

---
+++ C Programming/Switch /1)Vowel_or_Not.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter an Alphabet:  ");
+scanf("%c",&ch);
+switch(ch)
+{
+  case 'a':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'A':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'e':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'E':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'I':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'i':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'o':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'O':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'u':
+  printf("%c is a Vowel",ch);
+  break;
+  case 'U':
+  printf("%c is a Vowel",ch);
+  break;
+  default:
+  printf("%c is not a Vowel",ch);
+  break;
+}
+    return 0;
+}
