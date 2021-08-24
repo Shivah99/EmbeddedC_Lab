e1fab3ad90a935367e0757bbe0856a1f8fd1d59f
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-24 07:32:42 +0000

    Add new file

---
+++ C Programming/While_loop/5)calculator_y/n.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+int a,b,d,sum;
+char ch,op;
+while (ch=='y' || ch=='Y');
+{
+
+printf ("Enter the first number:");
+scanf ("%d",&a);
+printf ("Enter the second number:");
+scanf ("%d",&b);
+printf("Choose the operand(+,-,/,*): ");
+scanf(" %c", &op);
+switch(op)
+{
+  case '+':
+  d=a+b;
+  printf("\n%d is a Sum",d);
+  break;
+  case '-':
+  d=a-b;
+  printf("\n%d is a Sub",d);
+  break;
+  case '/':
+  d=a/b;
+  printf("%d is a div",d);
+  break;
+  case '*':
+  d=a*b;
+  printf("\n%d is a Mul",d);
+  break;
+}
+
+printf ("\n\nDo you want to repeat the operation Y/N: ");
+scanf (" %c", &ch);
+
+}
+return 0;
+}
