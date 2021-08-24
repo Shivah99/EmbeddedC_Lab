9d5764d5c1c76266b1beccd697f89f9764295eeb
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-24 11:28:12 +0000

    Update 5)calculator_y|n.c

--- C Programming/While_loop/5)calculator_y|n.c
+++ C Programming/While_loop/5)calculator_y|n.c
@@ -1,40 +1,46 @@
 #include<stdio.h>
 int main()
 {
-int a,b,d,sum;
+int a,b,d;
 char ch,op;
-while (ch=='y' || ch=='Y');
+printf("To continue enter y: ");
+scanf("%c",&ch);
+while(ch)
 {
-
+    if(ch=='y' || ch=='Y')
+    {
 printf ("Enter the first number:");
 scanf ("%d",&a);
 printf ("Enter the second number:");
 scanf ("%d",&b);
-printf("Choose the operand(+,-,/,*): ");
+printf("Choose the operand(+,-,/,): ");
 scanf(" %c", &op);
 switch(op)
 {
-  case '+':
-  d=a+b;
-  printf("\n%d is a Sum",d);
-  break;
-  case '-':
-  d=a-b;
-  printf("\n%d is a Sub",d);
-  break;
-  case '/':
-  d=a/b;
-  printf("%d is a div",d);
-  break;
-  case '*':
-  d=a*b;
-  printf("\n%d is a Mul",d);
-  break;
+case '+':
+d=a+b;
+printf("\n%d is a Sum",d);
+break;
+case '-':
+d=a-b;
+printf("\n%d is a Sub",d);
+break;
+case '/':
+d=a/b;
+printf("%d is a div",d);
+break;
+case '*':
+d=a*b;
+printf("\n%d is a Mul",d);
+break;
 }
-
 printf ("\n\nDo you want to repeat the operation Y/N: ");
 scanf (" %c", &ch);
-
 }
+else
+{
 return 0;
+}
+}
+
 }
