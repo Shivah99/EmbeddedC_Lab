b5cefc26648901298ae6f863d18f4966d9fb047e
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-30 11:17:23 +0000

    Add new file

---
+++ C Programming/Functions_1/3)IsPrime.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int IsPrime(int num);
+void main()
+{
+int num, i, result;
+printf("Enter a number: ");
+scanf("%d", &num);
+result = IsPrime(num);
+if(result=1)
+printf("The given number is prime.\n");
+else
+printf("The given number is not prime. \n");
+
+}
+int IsPrime(int num)
+{
+int i=1,c=0;
+while(i<=num)
+{
+if(num%i==0)
+c++;
+i++;
+}
+if(c==2)
+return 1;
+else
+return 0;
+}
