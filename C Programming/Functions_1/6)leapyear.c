3df1464751662e37bff073918fc27897ffa03a32
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-30 18:36:43 +0000

    Update 6)leapyear.c

--- C Programming/Functions_1/6)leapyear.c
+++ C Programming/Functions_1/6)leapyear.c
@@ -20,18 +20,9 @@ int main()
  
 int leap(int year)  
 {  
-    if(year % 100 == 0)  
-    {  
-        if(year % 400 == 0)  
-            return 1;  
+  
+   if(((year % 4 == 0)  && (year % 400 == 0))||((year % 4 == 0)&& (year % 100 != 0)))
+        return 1;
         else  
-            return 0;  
-    }  
-    else  
-    {  
-        if(year % 4 == 0)  
-            return 1;  
-        else  
-            return 0;  
-    }  
+        return 0;  
 } 
