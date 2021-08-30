c4d3c10e0f298a647ced1911f991f8ac98631a3b
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-08-30 12:26:28 +0000

    Add new file

---
+++ C Programming/Functions_1/4)alphanumeric.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int fun_alpha_num(char c)
+{
+    if ((c >= '0' & c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return 1;
+else
+    return 0;
+}
+int main()
+{
+    char c;
+    printf("Enter character: ");
+    scanf("%c", &c);
+    if (fun_alpha_num(c))
+        printf("%c is an alphanumeric character\n",c);
+    else
+        printf("%c is not an alphanumeric character\n",c);
+return 0;
+}
