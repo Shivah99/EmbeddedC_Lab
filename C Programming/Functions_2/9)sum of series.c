cd78692e25d8269a48f089af20337dbfac39c8ff
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-09-01 05:47:20 +0000

    Update 9)sum of series.c

--- C Programming/Functions_2/9)sum of series.c
+++ C Programming/Functions_2/9)sum of series.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int n = 5;
-    printf("%d", series(n));
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    series(n);
+   
     return 0;
 }
 int series(int n)
 {
-    int sum = 0, j = 1;
+    int sum = 0, j = 0;
     for (int i = 1; i <= n; i++) {
         sum = sum + j;
         j = (j * 10) + 1;
+         printf("%d+", j);
     }
-    return sum;
+    return 0;
 }
