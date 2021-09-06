0e9a262e76ab489dd2232b25cc2212fc565d2762
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-09-06 08:19:58 +0000

    Upload New File

---
+++ C Programming/Storage Classes/1)calculator 1/operations.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int add(int num1, int num2)
+{
+    return num1 + num2;
+}
+int sub(int num1, int num2)
+{
+    return num1 - num2;
+}
+int mult(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int div(int num1, int num2)
+{
+    return num1 / num2;
+}
+
