6f991c0c45acf383b611f6049eecde165f4912ce
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-09-10 18:49:01 +0000

    Update 5)integer to binary.c

--- C Programming/Arrays2/5)integer to binary.c
+++ C Programming/Arrays2/5)integer to binary.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,a[50];
+    int n,i,a[50],p;
     printf("Enter an integer: ");
     scanf("%d",&n);
+    p=n;
     for(i=0;n>0;i++)
     {
         a[i]=n%2;
         n=n/2;
     }
-printf("\nBinary of Given Number is= ");    
+printf("\nBinary of %d is= ",p);    
 for(i=i-1;i>=0;i--)      
 printf("%d",a[i]);    
 }
