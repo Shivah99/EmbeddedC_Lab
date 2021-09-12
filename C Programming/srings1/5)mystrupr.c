71eeb04519fb52848044a785c00e761909300a1e
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-09-12 19:09:30 +0000

    Update 5)mystrupr.c

--- C Programming/srings1/5)mystrupr.c
+++ C Programming/srings1/5)mystrupr.c
@@ -4,10 +4,10 @@ int main()
     char s1[100];
     scanf("%[^\n]100s",s1);
     printf("\nrandom Characters of string:%s",s1);
-    mystrlwr(s1);
+    mystrupr(s1);
     printf("\nLower characters of string: %s",s1);
 }
-void mystrlwr(char s[])
+void mystrupr(char s[])
 {
     int i;
     for (i=0;s[i]!='\0';i++)
