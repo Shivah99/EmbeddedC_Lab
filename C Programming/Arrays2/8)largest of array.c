d7a2f9c9424eac6ad2bb3ffa5e07ddffe47d6c1a
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-09-15 04:01:46 +0000

    Add new file

---
+++ C Programming/Arrays2/8)largest of array.c
@@ -0,0 +1,28 @@
+int main()
+{
+  int a[10], Size, i, Largest,;
+  
+  printf("\nPlease Enter the size of an array \n");
+  scanf("%d",&Size);
+  
+  printf("\nPlease Enter %d elements of an array: \n", Size);
+  for(i=0; i<Size; i++)
+   {
+     scanf("%d",&a[i]);
+   }   
+  
+  Largest = a[0];
+  for(i=1; i<Size; i++)
+   {
+    if(Largest<a[i])
+     {
+       Largest=a[i];
+       
+     }   
+   }
+ 
+  printf("\nLargest element in an Array = %d", Largest);
+  printf("\n The Largest element has occurred=%d",c);
+  
+  return 0;
+}
