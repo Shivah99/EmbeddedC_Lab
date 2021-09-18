726d8cc895e834958d5a2e9d203f3090b9ae668e
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-09-18 03:17:36 +0000

    Update 7)strings.c

--- C Programming/Lab Tests/7)strings.c
+++ C Programming/Lab Tests/7)strings.c
@@ -1,4 +1,10 @@
 //dt: 16/09/2021
+//Problem Statement # Write a program to read a numeric string and convert it to an integer ( Own implementation of atoi library function).Write three functions 
+//1 ) main function to read the string, validate it (if it is numeric or not) and call myatoi function, and print the return integer value.
+//2) myatoi function to Receive the numerical string as input argument and return the equivalent integer as output.
+//3) IsNumericString function to Validate the string if it is a valid numeric string or not. returns 1 if the entered string can be converted to an integer. Otherwise returns 0.
+//eg1., input a numeric string(read using %s) : 257 output integer(printed using %d): 257 
+//eg2: inpit a numeric string : 23k   output :  Entered string is not a valid numeric string.
 #include<stdio.h>
 #define size 100
 int myatoi(char s1[]);
