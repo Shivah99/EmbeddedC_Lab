fac465c02203e9c17f2eb3c5f49e9326b5dcd2f1
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-09-19 17:04:41 +0000

    Add new file

---
+++ C Programming/preproc 1/6)prime macro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define prime(num) while(i<=n) \
+{ \
+if(n%i==0)\
+{ \
+c++;\
+} \
+i++; } 
+
+int main()
+{
+int i=1,n,c=0;
+printf("Enter n:");
+scanf("%d",&n);
+prime(n)
+if(c==2)
+    printf("prime");
+else
+    printf("not prime");
+}
