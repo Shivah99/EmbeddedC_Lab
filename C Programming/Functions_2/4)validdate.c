e529f05baafd7740683cc713b5ef7dca31bb1913
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-10-19 14:11:26 +0000

    Update 4)validdate.c

--- C Programming/Functions_2/4)validdate.c
+++ C Programming/Functions_2/4)validdate.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int isleap(int y) {
-   if((y % 4 == 0) && (y % 100 != 0) && (y % 400 == 0))
+  if( ( (y%4 == 0) && (y%100 !=0) ) || ((y%4 == 0)&&(y%400 == 0)))
       return 1;
    else
       return 0;
@@ -9,11 +9,9 @@ int isleap(int y) {
 int validdate(int d, int m, int y) {
    if(y < 1900|| y > 9999)
       return 0;
-   if(m < 1 || m > 12)
-      return 0;
-   if(d < 1 || d > 31)
-      return 0;
-   if( m == 2 )
+   if(m >=1 || m <= 12)
+   {
+        if( m == 2 )
    {
       if(isleap(y)) {
          if(d <= 29)
@@ -22,6 +20,12 @@ int validdate(int d, int m, int y) {
             return 0;
          }
       }
+       return 0;
+   }
+     
+   if(d < 1 || d > 31)
+      return 0;
+  
       if ( m == 4 || m == 6 || m == 9 || m == 11 )
          if(d <= 30)
             return 1;
