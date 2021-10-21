715328c5de43882988e1b954d58a5be20ddbed27
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-10-21 10:58:27 +0000

    Update add.sh

--- LSP/shell scripting programs/add.sh
+++ LSP/shell scripting programs/add.sh
@@ -1,12 +1,10 @@
-# Write shell script that will add two nos, which are supplied as command line
- # argument, and if this two nos are not given show error and its usage
 
 #!/bin/sh
 
 echo "Enter two numbers to ADD"
 read num1
 read num2
-if [ $# !=  2 ]
+if [ $# -le  2 ]
 then
 	echo "$(( $num1 + $num2 ))"
 else
