e07b59c4f81e3d779ef1f2d81b001a32146fee96
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-10-27 14:30:10 +0000

    Update 3_framebuffer.c

--- LSP/Advanced File Operations/3_framebuffer.c
+++ LSP/Advanced File Operations/3_framebuffer.c
@@ -1,73 +1,30 @@
- #include <stdlib.h>
- #include <unistd.h>
- #include <stdio.h>
- #include <fcntl.h>
- #include <linux/fb.h>
- #include <sys/mman.h>
- #include <sys/ioctl.h>
+#include <linux/fb.h> // display sub system in kernel
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
- int main()
- {
-     int fbfd = 0;
-     struct fb_var_screeninfo vinfo;
-     long int screensize = 0;
-     char *fbp = 0;
-     int x = 0, y = 0;
-     long int location = 0;
+int main()
+{
+	int fd,ret;
+	struct fb_var_screeninfo temp;
+	fd = open("/dev/fb0",O_RDWR);
+	if (fd < 0){
+	perror("open fail");
+	exit(0);
+	}	
+	
+	ret = ioctl(fd,FBIOGET_VSCREENINFO,&temp);
+	if (ret < 0){
+	perror("ioctl fail");
+	exit(0);
+	}	
 
-     // Open the file for reading and writing
-     fbfd = open("/dev/fb0", O_RDWR);
-     if (fbfd == -1) {
-         perror("Error: cannot open framebuffer device");
-         exit(1);
-     }
-     printf("The framebuffer device was opened successfully.\n");
+	printf("xres:%d yres:%d bits_per_pixel:%d\n",temp.xres,temp.yres,temp.bits_per_pixel);
 
 
-     // Get variable screen information
-     if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
-         perror("Error reading variable information");
-         exit(3);
-     }
 
-     printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
-
-     // Figure out the size of the screen in bytes
-     screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
-
-     // Map the device to memory
-     fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,
-                        fbfd, 0);
-     if ((int)fbp == -1) {
-         perror("Error: failed to map framebuffer device to memory");
-         exit(4);
-     }
-     printf("The framebuffer device was mapped to memory successfully.\n");
-
-    // x = 100; y = 100;       // Where we are going to put the pixel
-
-     // Figure out where in memory to put the pixel
-     for (y = 0; y < 900; y++)
-         for (x = 0; x < 1440; x++) {
-
-             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                        (y+vinfo.yoffset) ;//* vinfo.line_length;
-
-             if (vinfo.bits_per_pixel == 32) {
-                 *(fbp + location) = 100;        // Some blue
-                 *(fbp + location + 1) = 25+(x-100)/2;     // A little green
-                 *(fbp + location + 2) = 20-(y-100)/5;    // A lot of red
-                 *(fbp + location + 3) = 0;      // No transparency
-             } else  { //assume 16bpp
-                 int b = 10;
-                 int g = (x-100)/6;     // A little green
-                 int r = 31-(y-100)/16;    // A lot of red
-                 unsigned short int t = r<<11 | g << 5 | b;
-                 *((unsigned short int*)(fbp + location)) = t;
-             }
-
-         }
-     munmap(fbp, screensize);
-     close(fbfd);
-     return 0;
- }
+}
