2caec52f11210980557150a9d82bd0a6f976dac4
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-10-27 09:25:24 +0000

    Update 1_mycat_mmap.c

--- LSP/Advanced File Operations/1_mycat_mmap.c
+++ LSP/Advanced File Operations/1_mycat_mmap.c
@@ -1,24 +1,33 @@
+#include<stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include<stdio.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include<stdlib.h>
 
 int main(int argc,char *argv[])
 {
 	int i,ret,fd;
 	struct stat temp;
-	char *buff,*temp2;
-	fd = open(argv[1],O_RDONLY);
-	ret = fstat(fd,&temp);
-	buff = (char *) mmap(0,temp.st_size,PROT_READ,MAP_PRIVATE,fd,0);
-	temp2=buff;
-	for(i=0;i<=temp.st_size;i++) {
-		printf("%c",*buff);
-		buff++;
-	}	
-
-	munmap(temp2,temp.st_size);	
+	char *temp1,*temp2;
+	fd=open(argv[1],O_RDONLY);
+	ret=fstat(fd,&temp);
+	if(ret<0)
+	{
+		perror("stat fail");
+		exit(0);
+	}
+	printf("Size of file is: %ld\n\n",temp.st_size);
+	temp1=(char *) mmap(0,temp.st_size,PROT_READ,MAP_PRIVATE,fd,0);
+	temp2=temp1;
+	for(i=0;i<=temp.st_size;i++)
+	{
+		printf("%c",*temp1);
+		temp1++;
+	}
+	munmap(temp2,temp.st_size);
+printf("\n");
 	return 0;
 }
