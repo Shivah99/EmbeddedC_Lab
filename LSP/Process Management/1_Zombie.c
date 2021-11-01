622606b5967eedebd1d74aca8c983ec30ae0ce2d
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-11-01 14:02:00 +0000

    Update 1_Zombie.c

---
+++ LSP/Process Management/1_Zombie.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+int main()
+{
+    pid_t pid;
+    char *message;
+    int n;
+
+    printf("fork program starting\n");
+    pid = fork();
+    switch(pid) 
+    {
+    case -1:
+        perror("fork failed");
+        exit(1);
+    case 0:
+        message = ("This is the child\n ");
+	printf ("child PID:%d Child PPID:%d\n",getpid(),getppid());
+        n = 3;
+        break;
+    default:
+        message = "This is the parent";
+	printf ("Parent PID:%d Parent PPID:%d\n",getpid(),getppid());
+	n = 5;
+	while(1);
+        break;
+    }
+
+    for(; n > 0; n--) {
+        puts(message);
+        sleep(1);
+    }
+  exit(0);
+}
