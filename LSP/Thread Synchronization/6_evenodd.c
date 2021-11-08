f140700a95977613a2051251eb729e6aa52cb2ae
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-11-08 17:06:30 +0000

    Add new file

---
+++ LSP/Thread Synchronization/6_evenodd.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <string.h>
+#define MAX_COUNT 20
+int count=0;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
+void* even(void* data)
+{
+    int tid = *((int*)data);
+    while (1)
+    {
+       pthread_mutex_lock(&lock);
+
+        while (count%2 == 0)
+           pthread_cond_wait(&condition, &lock);
+
+        if (count < MAX_COUNT)
+        {
+            count++;
+            printf("Thread %d, even= %d\n", tid, count);
+        }
+
+        // signal the other thread
+        pthread_cond_signal(&condition);
+
+        // check if time to exit
+        if (count >= MAX_COUNT)
+        {
+            pthread_mutex_unlock(&lock);
+            return NULL;
+        }
+	pthread_mutex_unlock(&lock);
+}}
+
+void* odd(void* data)
+{
+    int tid = *((int*)data);
+    while (1)
+    {
+       pthread_mutex_lock(&lock);
+
+        while (count%2 != 0)
+           pthread_cond_wait(&condition, &lock);
+
+        if (count < MAX_COUNT)
+        {
+            count++;
+            printf("Thread %d, odd %d\n", tid, count);
+        }
+
+        // signal the other thread
+        pthread_cond_signal(&condition);
+
+        // check if time to exit
+        if (count >= MAX_COUNT)
+        {
+            pthread_mutex_unlock(&lock);
+            return NULL;
+        }
+	pthread_mutex_unlock(&lock);
+}}
+
+   
+int main(int argc, char** argv)
+{
+    pthread_t thread1, thread2;
+    int tid[] = {1, 2};
+     
+    // create threads
+    pthread_create(&thread1, NULL, odd, &tid[0]);
+    pthread_create(&thread2, NULL, even, &tid[1]);
+    // wait for threads to finish their work
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+     
+    return 0;
+}
+  
