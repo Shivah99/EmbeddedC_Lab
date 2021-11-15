7c50fe3b471f6b426c1574a1a38722ad4694dd79
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-11-15 10:59:12 +0000

    Add new file

---
+++ C Programming/Strings 2/atoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int myAtoi(char* str)
+{
+    int res = 0;
+    for (int i = 0; str[i] != '\0'; ++i)
+        res = res * 10 + str[i] - '0';
+    return res;
+}
+
+int main()
+{
+    char str[] = "65";
+    int val = myAtoi(str);
+    printf("%d ", val);
+    return 0;
+}
