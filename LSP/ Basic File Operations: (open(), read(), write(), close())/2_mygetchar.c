fcf1fe7bd4d0c0990c1b7fcbf52bf29064004289
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-12-01 07:49:13 +0000

    Add new file

---
+++ LSP/ Basic File Operations: (open(), read(), write(), close())/2_mygetchar.c
@@ -0,0 +1,17 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <errno.h>
+#define BUFFERSIZE 20 
+int main(int argc, char* argv[])
+{
+char ch;
+char buffer[BUFFERSIZE];
+printf("Enter something \n");
+while (scanf("%c", &ch)==1)
+{
+        exit(1);
+}
+}
