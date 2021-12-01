c6ff71b03fad9f07f0916bfef86ac355274d70b5
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-12-01 18:03:00 +0000

    Update 6_trim.c

--- C Programming/Strings with Pointers/6_trim.c
+++ C Programming/Strings with Pointers/6_trim.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
- 
+#include<string.h>
+
 int main()
 {
     char s[100];  
-    int  i,j;
  
     printf("Enter a string : ");
     gets(s);
     
 	printf("Before trim :\t <%s>",s);
-	for(i=0;s[i]==' ';i++);             //trim leading white space 
+    printf("\nstrlen:%d\n",strlen(s));
+    
+    trim(s);
+    
+    printf("\nAfter trim:\t <%s>",s);
+    printf("\nstrlen:%d",strlen(s));
+    return 0;
+}
+void trim(char s[])
+{
+    int i,j;
+    
+	for(i=0;s[i]==32;i++);             //trim leading white space 
  
 	for(j=0;s[i];i++)                   //beginning of string without whitespaces 
 	{
@@ -18,10 +30,8 @@ int main()
 	s[j]='\0';
 	for(i=0;s[i]!='\0';i++)             //iterate till the string boecomes null
 	{
-		if(s[i]!=' ')
+		if(s[i]!=32)
 				j=i;
 	}
 	s[j+1]='\0';
-    printf("\nAfter trim:\t <%s>",s);
-    return 0;
 }
