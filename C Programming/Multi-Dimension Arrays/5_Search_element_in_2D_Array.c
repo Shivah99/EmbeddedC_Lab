e547239c79d63519fa84c4616ac3838c5b237263
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-12-07 17:47:15 +0000

    Add new file

---
+++ C Programming/Multi-Dimension Arrays/5_Search_element_in_2D_Array.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int s,m,n; 
+    int i,j,total=0;
+    printf("Enter no.of sales(s),no.of salesmen(m)");
+    scanf("%d%d",&s,&m);
+    printf("Enter the Element to search:");
+    scanf("%d",&n);
+    int a[s][m];
+    printf(" a[%d][%d]\n ",s,m); 
+       for (i=0;i<s;i++)    
+    {    
+        for (j=0;j<m;j++)    
+        {    
+            printf("a[%d][%d]:",i,j);                
+            scanf("%d",&a[i][j]);    
+        }    
+    }    
+    printf("\n***********matrix form**********\n");
+         for (i=0;i<s;i++)    
+    {   
+         printf("\n");
+        for (j=0;j<m;j++)    
+        {    
+            printf("%d\t",a[i][j]);                
+               
+        }    
+    } 
+ printf("\n***********ELEMENT POSITION**********\n");
+         for (i=0;i<s;i++)    
+    {   
+         printf("\n");
+        for (j=0;j<m;j++)    
+        {   
+            if(n==a[i][j])
+            printf("\nROW:%d\nCOLUMN:%d \na[%d][%d]=%d\n",i,j,i,j,n);                  
+        }    
+    }  
+}   
