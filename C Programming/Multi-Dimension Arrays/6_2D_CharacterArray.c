922553621958b561a9a30a6dbf4fe937c8a8dd77
Burgula Shivaji <shivaji.burgula.sb@gmail.com>
2021-12-08 09:09:05 +0000

    Update 6_2D_CharacterArray.c

--- C Programming/Multi-Dimension Arrays/6_2D_CharacterArray.c
+++ C Programming/Multi-Dimension Arrays/6_2D_CharacterArray.c
@@ -5,14 +5,10 @@ int main()
     printf("Enter the number of Strings and Size of each :\n");
     scanf("%d%d",&r,&c);
      char a[r][c];
-
-     // reading string from user
      printf("Enter %d strings of %d size:\n",r,c);
      for(i=0; i<r; i++)
      scanf("%s[^\n]",a[i]);
-
-     // dispaying strings
-     printf("\nEntered names are:\n");
+     printf("\nEntered Strings are:\n");
      for(i=0;i<r;i++)
      {
      printf("\n");
